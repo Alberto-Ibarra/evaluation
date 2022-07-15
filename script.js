8f83cd573899fc97878e2d3d59ee7872508d02a1
Alberto Ibarra <albib123@icloud.com>
2022-07-15 15:54:29 -0700

    commit

--- script.js
+++ script.js
@@ -1,37 +1,47 @@
+const baseUrl = "http://localhost:4232";
+const path = "movies";
 
+fetch([baseUrl, path].join("/"))
+    .then((response) => response.json())
+    .then((json) => {
+    console.log(json);
+    allMovie(json);
+    });
 
-    const baseUrl = 'http://localhost:4232'
-    const path = "movies"
-    
-
-        fetch([baseUrl, path].join("/"))
-        .then((response) => response.json())
-        .then(json => {
-            console.log(json);
-            showData(json)
-        })
-
-
-    const showData = (id) => {
-        const movieUl = document.querySelector('.movie_wrap')
-        let template = ""; //li template
-        id // represents item
-          .sort((a, b) => b.id - a.id) // compare function a n b/ ids below in delete function
-            .forEach((movie) => {
-            template +=  `
+const allMovie = (id) => {
+    const movieUl = document.querySelector(".movie_wrap");
+    let template = "";
+    id.sort((a, b) => b.id - a.id).forEach((movie) => {
+    template += `
             <li>
                 <img src="${movie.imgUrl}" alt="">
                 <p>${movie.name}</p>
                 <p>${movie.outlineInfo}</p>
             </li>
-            `; //// add edit function to update the text patch or put
-            });
-    
-        movieUl.innerHTML = template;
-    }
+            `;
+    });
 
+    movieUl.innerHTML = template;
+};
 
+function scroll() {
+    const ul = document.querySelector(".movie_wrap");
+    var scrollAmount = 0;
+    var scrollMin = 0;
+    var scrollMax = 100;
 
+    document.querySelector(".btn1").onclick = function () {
+    ul.scrollTo({
+        left: Math.max((scrollAmount += 1000), scrollMax),
+        behavior: "smooth",
+    });
+    };
 
-
-
+    document.querySelector(".btn2").onclick = function () {
+    ul.scrollTo({
+        left: Math.min((scrollAmount -= 1000), scrollMin),
+        behavior: "smooth",
+    });
+};
+}
+scroll();
